c5bf3186296ac3d67c3b977e7cdbab89f0133013
Keoni Mahelona <keoni@kmahelona.net>
2020-10-12 17:48:57 +1300

    Skip throttling for staff and superusers

--- corpora/corpora/base_settings.py
+++ corpora/corpora/base_settings.py
@@ -458,7 +458,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle',
+        'corpora.throttle.CorporaUserRateThrottle',
         'rest_framework.throttling.ScopedRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
