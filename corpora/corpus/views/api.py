65edcf6058974f2ebe60dbea4836c4c48df57ef1
Keoni Mahelona <kmahelona@users.noreply.github.com>
2020-10-28 15:28:01 +1300

    Add page_size field to paginations (#174)

--- corpora/corpus/views/api.py
+++ corpora/corpus/views/api.py
@@ -76,14 +76,17 @@ class ViewSetCacheMixin(object):
 
 class OneHundredResultPagination(pagination.PageNumberPagination):
     page_size = 100
+    page_size_query_param = 'page_size'
 
 
 class OneResultPagination(pagination.PageNumberPagination):
     page_size = 1
+    page_size_query_param = 'page_size'
 
 
 class TenResultPagination(pagination.PageNumberPagination):
     page_size = 10
+    page_size_query_param = 'page_size'
 
 
 class PutOnlyStaffReadPermission(permissions.BasePermission):
