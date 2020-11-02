c5c0f21b7fe5f276688d05043e0bcdca4d2c997a
Keoni Mahelona <keoni@kmahelona.net>
2020-11-02 21:40:02 +1300

    Added db settings and route for read replica

---
+++ corpora/corpora/db_routes.py
@@ -0,0 +1,11 @@
+import random
+from django.cong import settings
+
+
+class ReadRouter:
+
+    def db_for_read(self, model, **hints):
+        '''
+        A simple router to go between primary and read replica databases
+        '''
+        return random.choice(list(settings.DATABASES.keys()))
