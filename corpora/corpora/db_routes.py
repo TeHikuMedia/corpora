304cce79646d940cf24af898f4b89c66b4b58dc7
Keoni Mahelona <keoni@kmahelona.net>
2020-11-03 07:58:35 +1300

    allow relations on read router

--- corpora/corpora/db_routes.py
+++ corpora/corpora/db_routes.py
@@ -8,3 +8,10 @@ class ReadRouter:
         A simple router to go between primary and read replica databases
         '''
         return random.choice(list(settings.DATABASES.keys()))
+
+    def allow_relation(obj1, obj2, **hints):
+        '''
+        Always allow relations. This is needed for read only
+        select related.
+        '''
+        return True
